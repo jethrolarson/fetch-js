61286e42384bc0dfae2c177bb056efb08486596e
jethro larson <jethrolarson@sbgtv.com>
2016-08-17 14:02:27 -0700

    Made fetchJs return a promise and updated tests.

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-module.exports = function fetchJs (url, cb) {
-  var el = document.createElement('script')
-  var loaded
-  el.type = 'text\/javascript'
-  el.async = true
-  el.src = url
-  el.onerror = el.onload = function (err) {
-    if (err && err.type === 'error') return finish(err)
-    if (loaded || (el.readyState && !/^(c|loade)/.test(el.readyState))) return
-    return finish()
-  }
-  document.head.appendChild(el)
-  return el
+module.exports = function fetchJs (url) {
+  return new Promise(function (resolve, reject) {
+    var el = document.createElement('script')
+    var loaded
+    el.type = 'text\/javascript'
+    el.async = true
+    el.src = url
+    el.onerror = el.onload = function (err) {
+      if (err && err.type === 'error') return finish(err)
+      if (loaded || (el.readyState && !/^(c|loade)/.test(el.readyState))) return
+      return finish()
+    }
+    document.head.appendChild(el)
 
-  function finish (err) {
-    loaded = true
-    if (el.parentElement) el.parentElement.removeChild(el)
-    return cb && cb(err)
-  }
+    function finish (err) {
+      loaded = true
+      if (el.parentElement) el.parentElement.removeChild(el)
+      return err ? reject(err) : resolve(el)
+    }
+  })
 }
--- test/test-fetch-js.js
+++ test/test-fetch-js.js
@@ -11,59 +11,74 @@ describe('fetch-js', function () {
     window.hello = undefined
   })
   it('should execute a script and then trigger the callback', function (done) {
-    fetchJs('/base/test/fixtures/win.js', function (err) {
-      expect(err).to.be(undefined)
+    fetchJs('/base/test/fixtures/win.js').then(function () {
       expect(window.hello).to.eql('hello')
+      done()
+    }).catch(function () {
+      expect(false, 'error should not happen').to.be.ok
       done()
     })
   })
   it('should work cross domain', function (done) {
-    fetchJs(serverUrl + '/respond/window.hello="hello"', function (err) {
-      expect(err).to.be(undefined)
+    fetchJs(serverUrl + '/respond/window.hello="hello"').then(function () {
       expect(window.hello).to.eql('hello')
+      done()
+    }).catch(function () {
+      expect(false, 'error should not happen').to.be.ok
       done()
     })
   })
   it('should remove itself once complete', function (done) {
-    var el
-    el = fetchJs('/base/test/fixtures/win.js', function () {
+    fetchJs('/base/test/fixtures/win.js').then(function (el) {
       expect(el.parentElement).to.eql(null)
       done()
+    }).catch(function () {
+      expect(false, 'error should not happen').to.be.ok
+      done()
     })
-    expect(el.parentElement).to.eql(document.head)
   })
   it('should work multiple consecutive times with same file', function (done) {
-    fetchJs('/base/test/fixtures/win.js', function (err) {
-      expect(err).to.be(undefined)
+    fetchJs('/base/test/fixtures/win.js').then(function () {
       expect(window.hello).to.eql('hello')
       window.hello = undefined
-      fetchJs('/base/test/fixtures/win.js', function (err) {
-        expect(err).to.be(undefined)
-        expect(window.hello).to.eql('hello')
-        done()
-      })
+    }).then(function () {
+      return fetchJs('/base/test/fixtures/win.js')
+    }).then(function () {
+      expect(window.hello).to.eql('hello')
+      done()
+    }).catch(function () {
+      expect(false, 'error should not happen').to.be.ok
+      done()
     })
   })
   it('should work multiple consecutive times when file is cached', function (done) {
-    fetchJs(serverUrl + '/cache/respond/window.hello="hello"', function (err) {
-      expect(err).to.be(undefined)
+    fetchJs(serverUrl + '/cache/respond/window.hello="hello"').then(function () {
       expect(window.hello).to.eql('hello')
       window.hello = undefined
-      fetchJs(serverUrl + '/cache/respond/window.hello="hello"', function (err) {
-        expect(err).to.be(undefined)
-        expect(window.hello).to.eql('hello')
-        done()
-      })
+    }).then(function () {
+      return fetchJs(serverUrl + '/cache/respond/window.hello="hello"')
+    }).then(function () {
+      expect(window.hello).to.eql('hello')
+      done()
+    }).catch(function () {
+      expect(false, 'error should not happen').to.be.ok
+      done()
     })
   })
   it('should error if the script fails to load', function (done) {
-    fetchJs('/base/test/fixtures/i-dont-exist.js', function (err) {
+    fetchJs('/base/test/fixtures/i-dont-exist.js').then(function () {
+      expect(false, 'success should not happen').to.be.ok
+      done()
+    }).catch(function (err) {
       expect(err).not.to.be(undefined)
       done()
     })
   })
   it('should error if the script responds with a bad status', function (done) {
-    fetchJs(serverUrl + '/status/404', function (err) {
+    fetchJs(serverUrl + '/status/404').then(function () {
+      expect(false, 'success should not happen').to.be.ok
+      done()
+    }).catch(function (err) {
       expect(err).not.to.be(undefined)
       done()
     })
